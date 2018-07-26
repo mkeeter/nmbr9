79c0e2ed61ce4e9be597a9677102e952a82f25be
Matt Keeter <matt.j.keeter@gmail.com>
2018-07-26 19:29:57 -0400

    Combinatorial explosions are growing

--- src/stackup.rs
+++ src/stackup.rs
@@ -1,4 +1,6 @@
 use std::collections::HashSet;
+use std::fmt;
+use std::char::from_digit;
 
 use piece::{PIECE_AREA, UNIQUE_PIECE_COUNT};
 
@@ -9,9 +11,72 @@ use piece::{PIECE_AREA, UNIQUE_PIECE_COUNT};
 pub struct Stackup([usize; 10]);
 
 /*
+ *  Represents a selection of which pieces to put on which layer
+ *  Each layer stores a 10-digit ternary value, where each digit
+ *  means placing 0, 1, or 2 pieces of that digit's value.
+ */
+#[derive(Hash, Eq, PartialEq, Debug, Clone)]
+pub struct Layers([Layer; 10]);
+
+/*  Represents a single layer as a ternary value */
+#[derive(Hash, Eq, PartialEq, Clone, Copy)]
+pub struct Layer(u16);
+
+impl fmt::Debug for Layer {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Layer(")?;
+        for i in (0..10).rev() {
+            write!(f, "{}", from_digit(
+                    ((self.0 / (3 as u16).pow(i)) % 3) as u32, 3).unwrap())?;
+        }
+        return write!(f, ")");
+    }
+}
+
+fn choose(a: Layer, n: usize) -> HashSet<Layer> {
+    let mut out = HashSet::new();
+    if n == 0 {
+        out.insert(Layer(0));
+        return out;
+    }
+
+    for i in 0..10 {
+        let p = (3 as u16).pow(i);
+        let rem = (a.0 / p) % 3;
+
+        if rem > 0 {
+            for o in choose(Layer(a.0 - p), n - 1) {
+                out.insert(Layer(o.0 + p));
+            }
+        }
+    }
+    return out;
+}
+
+/*
  *  Generates all of the valid stackups that use all 20 pieces
  */
 impl Stackup {
+    pub fn to_layers(&self) -> Vec<Layers> {
+
+        let mut todo = Vec::new();
+        todo.push((Layers([Layer(0); 10]), Layer((3 as u16).pow(10) - 1)));
+
+        for i in 0..10 {
+            let mut next = Vec::new();
+            for (arr, rem) in todo.iter() {
+                for d in choose(*rem, self.0[i]) {
+                    let mut arr = arr.clone();
+                    arr.0[i] = d;
+                    next.push((arr, Layer(rem.0 - d.0)));
+                }
+            }
+            todo = next;
+        }
+
+        todo.iter().map(|(a, _)| a.clone()).collect()
+    }
+
     pub fn gen() -> Vec<Stackup> {
         let mut todo = Vec::new();
 
