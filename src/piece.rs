257f6b507fa84023e4c5a4dee8eb0760bfd1dfb7
Matt Keeter <matt.j.keeter@gmail.com>
2018-08-02 19:55:06 -0400

    Rearrange

--- src/piece.rs
+++ src/piece.rs
@@ -23,11 +23,11 @@ lazy_static! {
         for (i, p) in PIECES.iter().enumerate() {
             out[i] = p.count_ones();
         }
-        let total: u32 = out.iter().sum();
 
         // Piece 11 is a dummy piece that represents the whole world,
         // so it's bigger than the sum of all pieces that we could
         // possibly place.
+        let total: u32 = out.iter().sum();
         out[UNIQUE_PIECE_COUNT] = total * 2 + 1;
 
         return out;
